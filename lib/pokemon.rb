f7166bb647788db16e385164ab399c735e38becf
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:52:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,9 +5,9 @@ attr_accessor :name, :type, :db, :id
 def initialize(keywords)
 end
 
-  def self.save(name, type, db)
+def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
-  end
+end
   
   
 end
