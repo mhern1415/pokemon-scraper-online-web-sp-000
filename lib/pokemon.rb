c43a9c4a4100f8386f109c5f6e0b66bce5f2c0cb
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:53:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,6 @@ def self.find(num, db)
     new_pokemon.id = pokemon[0][0]
     new_pokemon.name = pokemon[0][1]
     new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
     return new_pokemon
 end
   
